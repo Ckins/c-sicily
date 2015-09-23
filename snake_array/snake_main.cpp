7e49b8d06a7be39a99e894fc7dd89827b114b522
Ckins <492159680@qq.com>
2015-09-23 22:08:20 +0800

    correct output

--- snake_array/snake_main.cpp
+++ snake_array/snake_main.cpp
@@ -10,6 +10,7 @@ int main() {
     int j = 0;
     int count = 1;
 
+    cout << count/2 << endl;
     for (i = 1; i <= n; i++) {
         if (i%2 != 0) {
             for (j = i/2; j < n-(i/2);j++) {
@@ -17,11 +18,11 @@ int main() {
                 count++;
             }
             for (j = 1+i/2; j < n-i/2; j++) {
-                a[j][n-i/2] = count;
+                a[j][n-i/2-1] = count;
                 count++;
             }
         } else {
-            for (j = n-i/2; j >= i/2-1; j--) {
+            for (j = n-i/2-1; j >= i/2-1; j--) {
                 a[n-i/2][j] = count;
                 count++;
             }
@@ -32,6 +33,12 @@ int main() {
         }
     }
 
+    for (i = 0;i < n;i++) {
+        for (j = 0; j < n; j++) {
+            cout << a[i][j] << "  ";
+        }
+        cout << endl;
+    }
     return 0;
 }
 
